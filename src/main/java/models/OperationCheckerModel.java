f46646d26feae95fca1b1c76bae751f8d8a5d799
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2024-06-02 17:24:45 +0400

    задание2

---
+++ src/main/java/models/OperationCheckerModel.java
@@ -0,0 +1,9 @@
+package models;
+
+import mock_map.ModelEnum;
+
+public class OperationCheckerModel extends MockModel {
+    public OperationCheckerModel(ModelEnum mockEnum) {
+        super(mockEnum);
+    }
+}
---
+++ src/main/java/models/PersonInfoModel.java
@@ -0,0 +1,9 @@
+package models;
+
+import mock_map.ModelEnum;
+
+public class PersonInfoModel extends MockModel {
+    public PersonInfoModel(ModelEnum mockEnum) {
+        super(mockEnum);
+    }
+}
